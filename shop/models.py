cb0304b10ddb3c19d9efa32f18a1b0e9113202f4
jod35 <jod35@test.com>
2020-10-08 15:58:13 +0300

    created views and their url patterns

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -15,6 +16,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.id, self.slug])
 
 
 class Product(models.Model):
@@ -35,3 +39,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_details', args=[self.id, self.slug])
