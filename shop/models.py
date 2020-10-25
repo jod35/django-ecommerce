394385d76523e2c779a79c2cde774befa3eaa13d
jod35 <jod35@test.com>
2020-10-25 12:14:05 +0300

    registered models with admin site

--- shop/models.py
+++ shop/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
     class Meta:
         ordering=('name',)
         verbose_name='category'
-        vaerbose_name_plural='categories'
+        verbose_name_plural='categories'
 
 
     def __str__(self):
@@ -21,7 +21,7 @@ class Category(models.Model):
 class Product(models.Model):
     category=models.ForeignKey(Category,on_delete=models.CASCADE,related_name='products')
     name=models.CharField(max_length=200,db_index=True)
-    slug=models.SlugField(max_lengh=200,db_index=True)
+    slug=models.SlugField(max_length=200,db_index=True)
     image=models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
     description=models.TextField(blank=True)
     price=models.DecimalField(max_digits=10,decimal_places=2)
